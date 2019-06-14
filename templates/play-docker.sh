da82a8840f8c1b87252ceca37bb807063077a835
glwlg <651971564@qq.com>
2019-06-14 15:35:53 +0800

    Update play-docker.sh

--- templates/play-docker.sh
+++ templates/play-docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then            
+    echo "Usage: sh $0 version#" 
+    exit -1                      
+fi
+
+ROCKETMQ_VERSION=$1
+
 RMQ_CONTAINER=$(docker ps -a|awk '/rmq/ {print $1}')
 if [[ -n "$RMQ_CONTAINER" ]]; then
    echo "Removing RocketMQ Container..."
@@ -16,10 +23,10 @@ echo "Starting RocketMQ nodes..."
 
 # Start nameserver
 # Start nameserver
-docker run -d -v `pwd`/data/namesrv/logs:/home/rocketmq/logs -v `pwd`/data/namesrv/store:/home/rocketmq/store --name rmqnamesrv rocketmqinc/rocketmq:ROCKETMQ_VERSION sh mqnamesrv
+docker run -d -v `pwd`/data/namesrv/logs:/home/rocketmq/logs -v `pwd`/data/namesrv/store:/home/rocketmq/store --name rmqnamesrv rocketmqinc/rocketmq:$ROCKETMQ_VERSION sh mqnamesrv
 
 # Start Broker
-docker run -d  -v `pwd`/data/broker/logs:/home/rocketmq/logs -v `pwd`/data/broker/store:/home/rocketmq/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" rocketmqinc/rocketmq:ROCKETMQ_VERSION sh mqbroker
+docker run -d  -v `pwd`/data/broker/logs:/home/rocketmq/logs -v `pwd`/data/broker/store:/home/rocketmq/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" rocketmqinc/rocketmq:$ROCKETMQ_VERSION sh mqbroker
 
 # Servive unavailable when not ready
 # sleep 20
